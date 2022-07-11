5f8f7936b563ecabbe920b4b044a483008d08108
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-11 15:06:38 +0500

    add SolutionTask7

---
+++ SolutionTask7/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка)
+Console.WriteLine("Введите число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int a = int.Parse(inputLine);
+    if (a % 2==0)
+    {
+    Console.Write($"Число {inputLine} является четным");  
+    }
+    else
+    {
+     Console.Write($"Число {inputLine}  является  не четным");
+    }
+ }
