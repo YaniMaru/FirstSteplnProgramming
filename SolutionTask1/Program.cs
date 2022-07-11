55a01def19cc9d0ba5f71f46dafcb174cbc1b1f6
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-11 12:07:17 +0500

    add SolutionTask1

---
+++ SolutionTask1/Program.cs
@@ -0,0 +1,26 @@
+﻿//Программа, которая принимает на вход 2 числа, и проверяет, является ли первое число квадратом второго
+string? inputLkneOne = Console.ReadLine();
+string? inputLkneTwo = Console.ReadLine();
+
+if(inputLkneOne != null && inputLkneTwo != null){
+    int inputNumberOne = int.Parse(inputLkneOne);
+    int inputNumberTwo = int.Parse(inputLkneTwo);
+
+    // if(inputNumberOne == inputNumberTwo * inputNumberTwo)
+    // {
+    //     Console.WriteLine("yes");
+    // }
+    // else
+    // {
+    //     Console.WriteLine("no")
+    // }
+
+    if(inputNumberTwo == Math.Sqrt(inputNumberOne))
+    {
+        Console.WriteLine("yes");
+    }
+    else
+    {
+        Console.WriteLine("no");
+    }
+}
