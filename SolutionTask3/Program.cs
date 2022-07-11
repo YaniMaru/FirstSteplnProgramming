31a2651289d957aa4d4e4397737e004f4959887b
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-11 12:16:16 +0500

    add SolutionTask3

---
+++ SolutionTask3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Программа, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N До N
+string? inputLkOne = Console.ReadLine();
+
+if(inputLkOne != null)
+{
+
+    int inputNumber = int.Parse(inputLkOne);
+
+    int startNumber = 1;
+
+    string lineOutput = "0";
+
+    while (startNumber <= inputNumber)
+    {
+        lineOutput = startNumber*(-1)+", "+lineOutput+", "+startNumber;
+        startNumber++;
+    }
+    
+    //LineOutput=LineOutput+inputNumber;
+
+    Console.WriteLine(lineOutput);
+}
