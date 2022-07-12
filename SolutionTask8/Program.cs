8d2040e8b9b83ac1f96211d0c076a0168328cc17
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-12 11:23:45 +0500

    add  SolutionTask8 correction

--- SolutionTask8/Program.cs
+++ SolutionTask8/Program.cs
@@ -1,34 +1,55 @@
 ﻿//Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 Console.WriteLine("Введите число: ");
 string? inputLkOne = Console.ReadLine();
-
 if(inputLkOne != null)
 {
 int inputNumber = int.Parse(inputLkOne);
-
- 
-//         for(int x = 2; x <= inputNumber; x++)       
-//         if (x % 2==0)             
-     
-//     Console.Write($"{x}, " );
-
-    string line = "";
-    int x = 2;
-    
-    if (inputNumber > 1)
-    {
-        while (x < inputNumber)
-        {
-            line = line + x + ", ";
-            x = x + 2;
-        }
-        if (inputNumber % 2 == 0)
-        {
-            line = line + inputNumber;
-        }
-        
-        Console.WriteLine(line);
-
-    }
+string line = "";
+int x = 2;
+if (inputNumber > 1)
+{
+while (x <= inputNumber-2)
+{
+line = line + x + ", ";
+x = x + 2;
 }
+if(x <= inputNumber)
+{
+line = line + x;
+}
+Console.WriteLine(line);
+}
+}
+
+
+
+
+// //Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+// Console.WriteLine("Введите число: ");
+// string? inputLkOne = Console.ReadLine();
+
+// if(inputLkOne != null)
+// {
+// int inputNumber = int.Parse(inputLkOne);
+
+//     string line = "";
+//     int x = 2;
+    
+//     if (inputNumber > 1)
+//     {
+//         while (x < inputNumber)
+//         {
+//             line = line + x + ", ";
+//             x = x + 2;
+//         }
+//         if (inputNumber % 2 == 0)
+//         {
+//             line = line + inputNumber;
+//         }
+        
+//         Console.WriteLine(line);
+
+//     }
+// }
+
 
