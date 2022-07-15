d442cc9e8c782f26fe49088cc494cdd6670a0b12
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-15 14:27:32 +0500

    add SolutionTask10

---
+++ SolutionTask10/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе
+//показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число: ");
+string? inputLine = Console.ReadLine();
+
+if (inputLine != null)
+{
+    int a = int.Parse(inputLine);
+    int b = (a % 100) / 10;
+
+    Console.WriteLine(b);
+}
+
+
+// Console.WriteLine("Введите трехзначное число: ");
+// string? inputLine = Console.ReadLine();
+// if (inputLine != null)
+// {
+//     string stringNum = inputLine.ToString();
+//     Console.Write(stringNum[1]);
+// }
+
