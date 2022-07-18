f5c14f13ccef01f82990260c786d6d8136a68f93
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-18 17:13:27 +0500

    add SolutionTask22

---
+++ SolutionTask22/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программa, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+string? inputLineN = Console.ReadLine();
+
+if (inputLineN != null)
+{
+    int numberN = int.Parse(inputLineN);
+
+    string lineN = "";
+    string lineNN = string.Empty;
+    int s = 1;
+    while (s <= numberN)
+    {
+        lineN = lineN + s + " ";
+        lineNN = lineNN + s * s + " ";
+        s++;
+    
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNN);
+}
+
+
