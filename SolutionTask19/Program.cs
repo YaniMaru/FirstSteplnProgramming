70d1fd3c7f5606e1736541aae012e20c620b92b2
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-19 15:38:00 +0500

    add SolutionTask19,21

---
+++ SolutionTask19/Program.cs
@@ -0,0 +1,53 @@
+﻿//  Программа, которая принимает на вход пятизначное число и проверяет,является ли оно палиндромом.
+
+void Palindrom(int num)
+{
+    int D1 = num / 10000;
+    int D2 = (num / 1000) % 10;
+    int D3 = (num / 100) % 10;
+    int D4 = (num / 10) % 10;
+    int D5 = num % 10;
+
+    if (D1 == D5 && D2 == D4)
+    {
+        Console.WriteLine($"Число {num} является палиндромом");
+    }
+    else
+    {
+        Console.WriteLine($"Число {num}  не является  палиндромом");
+    }
+}
+
+void Variant2()
+{
+Console.WriteLine("Введите пятизначное число: ");
+string? inputLine = Console.ReadLine();
+if (inputLine != null)
+{
+    char[] arr = inputLine.ToCharArray();
+    if (arr.Length >= 5)
+    {
+        if ((arr[0] == arr[4]) && (arr[1] == arr[3]))
+        {
+            Console.Write($"Число {inputLine} является палиндромом");
+        }
+        else
+        {
+            Console.WriteLine($"Число {inputLine}  не является  палиндромом");
+        }
+    }
+}
+}
+
+
+Console.WriteLine("Введите пятизначное число: ");
+string? inputNumber = Console.ReadLine();
+
+if (inputNumber != null)
+{
+    int num = int.Parse(inputNumber);
+    Palindrom(num);
+}
+
+
+Variant2();
--- SolutionTask21/Program.cs
+++ SolutionTask21/Program.cs
@@ -1,11 +1,10 @@
-﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между
-// ними в 2D пространстве.
-
-
+﻿// Программа, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
 int coordXPointA;
 int coordYPointA;
 int coordXPointB;
 int coordYPointB;
+int coordZPointA;
+int coordZPointB;
 double lengthAB;
 
 // Считывает координаты точек А и В
@@ -18,18 +17,23 @@ void readDataOfPoint()
     Console.WriteLine("Введите координату Y точки А");
     coordYPointA = int.Parse(Console.ReadLine());
 
+    Console.WriteLine("Введите координату Z точки А");
+    coordZPointA = int.Parse(Console.ReadLine());
+
     Console.WriteLine("Введите координату X точки B");
     coordXPointB = int.Parse(Console.ReadLine());
 
     Console.WriteLine("Введите координату Y точки B");
     coordYPointB = int.Parse(Console.ReadLine());
+
+    Console.WriteLine("Введите координату Z точки B");
+    coordZPointB = int.Parse(Console.ReadLine());
 }
 
 // Вычисляет расстояние между точками А и В
 void conculateLengtAB()
 {
-    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2));
-                         
+    lengthAB = Math.Sqrt(Math.Pow((coordXPointA - coordXPointB), 2) + Math.Pow((coordYPointA - coordYPointB), 2)+ Math.Pow((coordZPointA - coordZPointB), 2));
 }
 
 readDataOfPoint();
