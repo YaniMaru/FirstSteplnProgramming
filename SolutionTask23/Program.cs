43f9d73898f712ecbc4dd3d57fd3981ddcca91b7
Yanina Maruseva <yanina-ts@mail.ru>
2022-07-20 10:09:13 +0500

    correction

--- SolutionTask23/Program.cs
+++ SolutionTask23/Program.cs
@@ -1,27 +1,27 @@
 ﻿// Программа, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//Метод возведения в степень "3" числа N
+void Cube(int numberN)
+{
+    string lineN = "";
+    string lineNNN = string.Empty;
+    int s = 1;
+    while (s <= numberN)
+    {
+        lineN = lineN + s + " ";
+        lineNNN = lineNNN + s * s * s + " ";
+        s++;
+    }
+    Console.WriteLine(lineN);
+    Console.WriteLine(lineNNN);
+}
+
 Console.WriteLine("Введите число: ");
 string? inputLineN = Console.ReadLine();
-//Метод возведения в степень "3" числа N
-void Cube()
+if (inputLineN != null)
 {
-    if (inputLineN != null)
-    {   
-        int numberN = int.Parse(inputLineN);
-        string lineN = "";
-        string lineNNN = string.Empty;
-        int s = 1; 
-        while (s <= numberN)
-        {
-            lineN = lineN + s + " ";
-            lineNNN = lineNNN + s * s * s + " ";
-            s++;
-        }
-        Console.WriteLine(lineN);
-        Console.WriteLine(lineNNN);
-    }
+    int numberN = int.Parse(inputLineN);
+    Cube(numberN);
 }
-Cube();
-
 
 
 
