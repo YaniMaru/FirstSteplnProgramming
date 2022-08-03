cc97c7ed527a9f8174ebf34478e555de2f77abcd
Yanina Maruseva <yanina-ts@mail.ru>
2022-08-03 13:45:25 +0500

    add mulRec

--- SolutionTask28/Program.cs
+++ SolutionTask28/Program.cs
@@ -4,12 +4,25 @@
 Console.Write("Введите число: ");
 int inputNum = int.Parse(Console.ReadLine());
 Console.WriteLine(sumNums(inputNum));
+Console.WriteLine(mulRec(inputNum));
 int sumNums(int num)
 {
     int sum = 1;
-    for(int i = 1; i <= inputNum; i++)
+    for (int i = 1; i <= inputNum; i++)
     {
         sum *= i;
     }
     return sum;
+}
+
+int mulRec(int num)
+{
+    if (num==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return num*mulRec(num-1);
+    }
 }
