9fc65d2a23d0683dd9b64788bb2d65cb2874fcb1
Yanina Maruseva <yanina-ts@mail.ru>
2022-08-03 13:04:55 +0500

    add SolutionTask28

---
+++ SolutionTask28/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число N и выдаёт произведение 
+//чисел от 1 до N. Например: 4 -> 24    5 -> 120
+
+Console.Write("Введите число: ");
+int inputNum = int.Parse(Console.ReadLine());
+Console.WriteLine(sumNums(inputNum));
+int sumNums(int num)
+{
+    int sum = 1;
+    for(int i = 1; i <= inputNum; i++)
+    {
+        sum *= i;
+    }
+    return sum;
+}
