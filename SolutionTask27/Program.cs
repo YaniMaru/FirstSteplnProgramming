18e86fc88d30138de2a0ba9cd557fa04db0147bf
Yanina Maruseva <yanina-ts@mail.ru>
2022-08-04 13:59:00 +0500

    add SolutionTask27

---
+++ SolutionTask27/Program.cs
@@ -0,0 +1,26 @@
+﻿/*----------------------------------------------------------------------------------
+ Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+ 452 -> 11   82 -> 10  9012 -> 12
+----------------------------------------------------------------------------------*/
+
+Console.Write("Введите : ");
+string inputLine = Console.ReadLine() ?? "";
+int inputNumber = int.Parse(inputLine);
+
+//method for calculating the sum of all digits in a number
+int Calculate()
+{
+    int sumOfDigits = 0;
+    while (inputNumber > 0)
+    {
+        sumOfDigits = sumOfDigits + (inputNumber % 10);
+        inputNumber = inputNumber / 10;
+
+    }
+    return sumOfDigits;
+}
+
+int result = Calculate();
+Console.WriteLine("Сумма цифр в числе: " + result);
+
+
