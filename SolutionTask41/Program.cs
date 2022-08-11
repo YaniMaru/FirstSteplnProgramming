a168c8e60e7c144906953ff59937b42566d15247
Yanina Maruseva <yanina-ts@mail.ru>
2022-08-11 21:17:36 +0500

    add dz 41,43

---
+++ SolutionTask41/Program.cs
@@ -0,0 +1,45 @@
+﻿/*---------------------------------------------------------------------------------
+№41 Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл
+пользователь.
+0, 7, 8, -2, -2 -> 2                           -1, -7, 567, 89, 223-> 3
+---------------------------------------------------------------------------------*/
+Console.Clear();
+int m;
+PrintResult(ColculateTask(Read()));
+
+//метод чтения данных
+int[] Read()
+{
+    Console.WriteLine($"Сколько всего чисел будет введено?: ");
+    m = int.Parse(Console.ReadLine() ?? ""); ;
+    int[] array = new int[m];
+    int i = 0;
+    Console.WriteLine("Введите числа. ");
+    while (i < m)
+    {
+        Console.Write($"{i + 1} число: ");
+        array[i] = int.Parse(Console.ReadLine() ?? "");
+        i++;
+    }
+    return array;
+}
+//метод вычисления
+int ColculateTask(int[] array)
+{
+    int countPosNum = 0;
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] > 0)
+        {
+            countPosNum++;
+        }
+    return countPosNum;
+}
+//метод печати
+void PrintResult(int countPosNum)
+{
+    Console.WriteLine($"Кол-во чисел больше нуля: {countPosNum} ");
+}
+
+
+
+
---
+++ SolutionTask43/Program.cs
@@ -0,0 +1,41 @@
+﻿/*---------------------------------------------------------------------------------
+ №43 Напишите программу, которая найдёт точку пересечения двух прямых, заданных
+уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются
+пользователем.
+b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+---------------------------------------------------------------------------------*/
+Console.Clear();
+double b1;
+double k1;
+double k2;
+double b2;
+double y;
+double x;
+
+Read();
+ColculateTask();
+Print();
+
+//метод чтения данных
+void Read()
+{
+    Console.Write("Введите b1: ");
+    b1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k1: ");
+    k1 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите b2: ");
+    b2 = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Введите k2: ");
+    k2 = Convert.ToDouble(Console.ReadLine());
+}
+//метод вычисления
+void ColculateTask()
+{
+    x = (b2 - b1) / (k1 - k2);
+    y = k1 * (b2 - b1) / (k1 - k2) + b1;
+}
+//метод печати
+void Print()
+{
+    Console.WriteLine($"Пересечение в точке: ({x}; {y})");
+}
