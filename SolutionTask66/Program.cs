0d7340673811922cbafbc8fb59f0b62b8396f8f1
Yanina Maruseva <yanina-ts@mail.ru>
2022-08-23 11:44:47 +0500

    add dz 64,66,68

---
+++ SolutionTask66/Program.cs
@@ -0,0 +1,42 @@
+﻿/*---------------------------------------------------------------------------------
+Задача 66:
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+---------------------------------------------------------------------------------*/
+
+Console.Clear();
+int numberM = ReadData("Задайте число M");
+int numberN = ReadData("Задайте число N");
+int sum = 0;
+Result(numberM, numberN);
+
+//метод считывания данных
+int ReadData(string line)
+{
+    Console.WriteLine(line);
+    return int.Parse(Console.ReadLine() ?? "");
+}
+// метод нахождения суммы через рекурсию
+void sumOfNaturalNumbers(int m, int n)
+{
+    if (m - 1 == n) return;
+    sum += m;
+    sumOfNaturalNumbers(m + 1, n);
+}
+//вывод результата
+void Result(int m, int n)
+{
+    if (m < n)
+    {
+        sumOfNaturalNumbers(m, n);
+        Console.WriteLine($"Сумма чисел от {m} до {n} равна {sum}");
+    }
+    else
+    {
+        sumOfNaturalNumbers(n, m);
+        Console.WriteLine($"Сумма чисел от {n} до {m} равна {sum}");
+    }
+
+}
---
+++ SolutionTask68/Program.cs
@@ -0,0 +1,30 @@
+﻿/*---------------------------------------------------------------------------------
+Задача 68:
+Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два
+неотрицательных числа m и n.
+
+m = 2, n = 3 -> A(m,n) = 29
+------------------------------------------------------------------------------------*/
+
+Console.Clear();
+uint numberM = ReadData("Задайте положительное число M");
+uint numberN = ReadData("Задайте положительное число N");
+Console.WriteLine(AckermanFunction(numberN, numberM));
+//метод считывания данных
+uint ReadData(string line)
+{
+    Console.WriteLine(line);
+    return uint.Parse(Console.ReadLine() ?? "");
+}
+
+// функция Аккермана
+uint AckermanFunction(uint n, uint m)
+{
+    if (n == 0)
+        return m + 1;
+    else
+      if ((n != 0) && (m == 0))
+        return AckermanFunction(n - 1, 1);
+    else
+        return AckermanFunction(n - 1, AckermanFunction(n, m - 1));
+}
